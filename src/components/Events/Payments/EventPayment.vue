bf00a987f97b613e2dbd678807556272dda68b81
Amaka Anyanwu <vanessapeters@126@gmail.com>
2021-01-10 19:45:02 +0100

    add styles

--- src/components/Events/Payments/EventPayment.vue
+++ src/components/Events/Payments/EventPayment.vue
@@ -1,34 +1,71 @@
 <template>
   <div>
-    <Header />
-    <div class="page-entry page--transition">
-      <div class="page-entry--wrap">
-        <div class="page-preview container">
-          <div class="fadeInUp animated">
-            <div class="row">
-              <div class="page-layout---card">
-                <h1 class="color-dark text-xlbold">
-                  The best events happening now.
-                </h1>
-                <div
-                  class="card-single--event"
-                  v-for="item in array"
-                  :key="item.id"
-                >
-                  <router-link
-                    exact
-                    :to="{
-                      name: 'EventDetails',
-                      path: `/events/${item.title}`,
-                      params: { title: item.title },
-                    }"
-                    class="card-event"
-                  >
-                    <SingleEvent v-bind:event="item" v-bind:key="item.id" />
-                  </router-link>
-                </div>
+    <div class="fadeInUp animated">
+      <div class="payments-page--wrap">
+        <div class="flex">
+          <div class="ticket-wrap left">
+            <!-- <div class="container flex"> -->
+              <div class="close-button--wrap flex mb-5">
+                <button class="button--primary button-round">
+                  <img src="../../../assets/img/Close.svg" class="mr-1" />Close
+                </button>
               </div>
-            </div>
+              <div>
+                <h1>The Nathan Cole Experience</h1>
+                <h6 class="text-capitalize color-grey text-normal">
+                  8th February 2019
+                </h6>
+              </div>
+              <div class="ticket--price--wrap">
+                <div class="flex justify-between align-center ticket--bordered">
+                  <p class="text-md text-normal color-dark">Regular</p>
+                  <p class="text-md text-normal color-dark">N5000</p>
+                  <div class="flex align-center">
+                    <button class="mr-2">
+                      <img src="../../../assets/img/deduct-item.svg" />
+                    </button>
+                    <p class="text-md text-normal color-dark">2</p>
+                    <button class="ml-2">
+                      <img src="../../../assets/img/add-item.svg" />
+                    </button>
+                  </div>
+                </div>
+                <div class="flex justify-between align-center ticket--bordered">
+                  <p class="text-md text-normal color-dark">Regular</p>
+                  <p class="text-md text-normal color-dark">N5000</p>
+                  <div class="flex align-center">
+                    <button class="mr-2">
+                      <img src="../../../assets/img/deduct-item.svg" />
+                    </button>
+                    <p class="text-md text-normal color-dark">2</p>
+                    <button class="ml-2">
+                      <img src="../../../assets/img/add-item.svg" />
+                    </button>
+                  </div>
+                </div>
+                <div class="flex justify-between align-center ticket--bordered">
+                  <p class="text-md text-normal color-dark">Regular</p>
+                  <p class="text-md text-normal color-dark">N5000</p>
+                  <div class="flex align-center">
+                    <button class="mr-2">
+                      <img src="../../../assets/img/deduct-item.svg" />
+                    </button>
+                    <p class="text-md text-normal color-dark">2</p>
+                    <button class="ml-2">
+                      <img src="../../../assets/img/add-item.svg" />
+                    </button>
+                  </div>
+                </div>
+                <p class="text-xs mt-4 color-grey--3">
+                  Ticket sales ends on 22nd November 2019
+                </p>
+              </div>
+            <!-- </div> -->
+          </div>
+          <div class="order-summary--wrap">
+            <h6 class="text-capitalize color-grey text-normal">
+              Order summary
+            </h6>
           </div>
         </div>
       </div>
@@ -37,15 +74,9 @@
 </template>
 
 <script>
-import Header from "../Header";
-import SingleEvent from "./SingleEventCard";
-
 export default {
   name: "EventPayment",
-  components: {
-    Header,
-    SingleEvent,
-  },
+  components: {},
   data() {
     return {
       events: [],
@@ -57,8 +88,6 @@ export default {
   //    watch: {
   //     '$route': 'fetchData'
   //   },
-  async fetchData() {
-   
-  },
+  async fetchData() {},
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -5,35 +5,36 @@ Vue.use(VueRouter);
 
 const AllEvents = () => import("@/components/Events/AllEvents");
 const EventDetails = () => import("@/components/Events/EventDetails");
+const EventPayment = () => import("@/components/Events/Payments/EventPayment");
 const Hello = () => import("@/components/HelloWorld");
 
 export const router = new VueRouter({
-  mode: "history",
-  routes: [
-    {
-      path: "/events",
-      name: "AllEvents",
-      component: AllEvents,
-    },
-    {
-      path: "/no-tickets",
-      name: "NoTickets",
-      component: Hello,
-    },
-    {
-      // path: "/events/:title",
-      path: "/event",
-      name: "EventDetails",
-      // props: true,
-      component: EventDetails,
-    },
-    {
-      // path: "/events/:title",
-      path: "/payment",
-      name: "EventDetails",
-      // props: true,
-      component: EventDetails,
-    },
-    { path: "/", redirect: "/events" },
-  ],
-});
+         mode: "history",
+         routes: [
+           {
+             path: "/events",
+             name: "AllEvents",
+             component: AllEvents,
+           },
+           {
+             path: "/no-tickets",
+             name: "NoTickets",
+             component: Hello,
+           },
+           {
+             // path: "/events/:title",
+             path: "/event",
+             name: "EventDetails",
+             // props: true,
+             component: EventDetails,
+           },
+           {
+             // path: "/events/:title",
+             path: "/payment",
+             name: "EventPayment",
+             // props: true,
+             component: EventPayment,
+           },
+           { path: "/", redirect: "/events" },
+         ],
+       });
